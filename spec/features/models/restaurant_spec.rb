57e018675608b8a19c56be92b31e52f640cdd18e
elliotlewis8923 <elewis8923@gmail.com>
2014-10-03 21:47:11 +0100

    passed model test for unique restaurant name

--- spec/features/models/restaurant_spec.rb
+++ spec/features/models/restaurant_spec.rb
@@ -8,6 +8,12 @@ describe 'Restaurant' do
 			Restaurant.create(name: 'hi', description: 'weeeeeeeeeeee')
 			expect(Restaurant.count).to eq 0
 		end
+
+		it 'must have a unique name' do
+			Restaurant.create(name: 'Chicken Cottage', description: 'reconstituted pigeon meat')
+			Restaurant.create(name: 'Chicken Cottage', description: 'yep you heard')
+			expect(Restaurant.count).to eq 1
+		end
 	end
 
 end
