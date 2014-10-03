1b9d105b2e976fa0e21555bcd55e78d4b19890b5
elliotlewis8923 <elewis8923@gmail.com>
2014-10-03 18:39:54 +0100

    created reviews model, updated paths and created association with restaurants

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -1,11 +1,28 @@
 require 'rails_helper'
 
 describe 'restaurants' do
-    context 'no restaurants have been added' do
-        it 'should display a prompt to add a restaurant' do
-            visit '/restaurants'
-            expect(page).to have_content 'No restaurants'
-            expect(page).to have_link 'Add a restaurant'
-        end
+  
+  context 'no restaurants have been added' do
+    
+    it 'should display a prompt to add a restaurant' do
+      visit '/restaurants'
+      expect(page).to have_content 'No restaurants'
+      expect(page).to have_link 'Add a restaurant'
     end
+  end
+
+  context 'restaurants have been added' do
+	  
+	  before do
+	      @restaurant = Restaurant.create(name: 'KFC', description: 'meh')
+	  end
+
+	  it 'should display restaurants' do
+	    visit '/restaurants'
+	    puts @restaurant.inspect
+	    expect(page).to have_content('KFC')
+	    expect(page).not_to have_content('No restaurants yet')
+	  end
+	end
+
 end
